9e80c246d8bc02665adec8e495264631462aa5de
gabriel souza <ghdesouza@gmail.com>
2016-06-05 15:44:27 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,107 +1,183 @@
 #include <GL/glut.h>
-#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int width = 800, height = 600;
-int last_x, last_y;
-float posicaoCameraZ = 45.0;
-float direcaoVisaoHorizontal = 0, posicaoCameraX = 0.0, posicaoCameraY = 0.0, alturaVisao = 0.0;
+float angle = 0, raio = 0.8;
+int enableMenu = 0;
+int dist = 25;
+int proj = 0;
+int width = 640;
+int height = 640;
+float viewangle = 40.0f;
+float distanciaOrigem = 30.0;
+float rotationX = 38.0, rotationY = 22.0;
+int   last_x, last_y;
 
-void init(void);
+void init (void);
+void reshape( int w, int h);
 void display(void);
-void reshape (int w, int h);
-void keyboard (unsigned char key, int x, int y);
+void desenhaEixos();
+void projec();
+void orthog();
 void mouse(int button, int state, int x, int y);
-void motion(int x, int y);
+void motion(int x, int y );
+void keyboard(unsigned char key, int x, int y);
 
 int main(int argc, char** argv){
-    glutInit(&argc, argv);
-    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize (width, height);
-    glutInitWindowPosition (100, 100);
-    glutCreateWindow ("Desenvolvimento 2");
+   glutInit(&argc, argv);
+   glutInitDisplayMode (GLUT_DOUBLE|GLUT_DEPTH|GLUT_RGB);
+   glutInitWindowSize (width, height);
+   glutInitWindowPosition (100, 100);
+   glutCreateWindow ("Projeções em OpenGL");
+   init ();
+   glutMouseFunc( mouse );
+   glutKeyboardFunc(keyboard);
+   glutMotionFunc( motion );
+   glutReshapeFunc( reshape );
+   glutDisplayFunc(display);
+   glutMainLoop();
 
-    init ();
-    glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    glutMouseFunc( mouse );
-    glutMotionFunc( motion );
-    glutKeyboardFunc(keyboard);
-    glutMainLoop();
    return 0;
 }
 
-void init(void){
+void init (void){
+   // selecionar cor de fundo (preto)
    glClearColor (0.0, 0.0, 0.0, 0.0);
-   glShadeModel (GL_SMOOTH);
 
-   glEnable(GL_LIGHT0);                  
-   glEnable(GL_COLOR_MATERIAL);          
-   glColorMaterial(GL_FRONT, GL_DIFFUSE);
+   glEnable(GL_LIGHT0);                   // habilita luz 0
+   glEnable(GL_COLOR_MATERIAL);           // Utiliza cor do objeto como material
+   glColorMaterial(GL_FRONT_AND_BACK, GL_DIFFUSE);
 
-   glEnable(GL_LIGHTING);               
-   glEnable(GL_DEPTH_TEST);               
-   glEnable(GL_CULL_FACE);                
+	GLfloat light0_position[] = {2.0f, 2.0f, 5.0f, 0.0f};
+   glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
+
+   glEnable(GL_LIGHTING);                 // Habilita luz
+   glEnable(GL_DEPTH_TEST);               // Habilita Z-buffer
+   glEnable(GL_CULL_FACE);                // Habilita Backface-Culling
+}
+
+void reshape( int w, int h){
+   width = w;
+   height = h;
+   glViewport(0,0,w,h);
+
+   glutPostRedisplay();
 }
 
 void display(void){
-   glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+   // Limpar todos os pixels
+   glClear (GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
-   glMatrixMode (GL_PROJECTION);
-   glLoadIdentity ();
-   gluPerspective(60.0, (GLfloat) width/(GLfloat) height, 1.0, 200.0);
+   // inicializar sistema de projeção
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+
+   if(!proj) orthog();
+   else projec();
+
+   desenhaEixos();
+
+   glutSwapBuffers ();
+   glutPostRedisplay();
+}
+
+void desenhaEixos(){
+   glDisable(GL_LIGHTING);
+   glColor3f (0.0, 1.0, 0.0);
+   glBegin(GL_LINES);
+      glColor3f(1.0, 0.0, 0.0);
+      glVertex3f (-dist, 0.0, 0.0);
+      glVertex3f ( dist, 0.0, 0.0);
+      glColor3f(0.0, 1.0, 0.0);
+      glVertex3f (0.0, -dist, 0.0);
+      glVertex3f (0.0,  dist, 0.0);
+      glColor3f(0.0, 0.0, 1.0);
+      glVertex3f (0.0, 0.0, -dist);
+      glVertex3f (0.0, 0.0,  dist);
+   glEnd();
+   glEnable(GL_LIGHTING);
+}
+
+void projec(){
+    gluPerspective(40.0f,(GLfloat)width/(GLfloat)height,0.1f,200.0f);	// Calculate The Aspect Ratio Of The Window
 
    glMatrixMode(GL_MODELVIEW);
-   glLoadIdentity ();
-   gluLookAt (posicaoCameraX, posicaoCameraY, posicaoCameraZ, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+   glLoadIdentity();
 
-      glColor3f (0.80, 0.60, 0.0);
-      glutSolidCube(10);
+   gluLookAt (0.0, 0.0, distanciaOrigem, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
-   glutSwapBuffers();
+   // Movimenta a camera em torno do objeto
+   // Luz fica fixa na posição (0, 0, 5) inicialmente
+   glRotatef( rotationY, 1.0, 0.0, 0.0 );
+   glRotatef( rotationX, 0.0, 1.0, 0.0 );
+
+   glPushMatrix();
+      glColor3f (0.283, 0.456, 0.518);
+      glutSolidCube(5);
+
+      glColor3f (1.0, 1.0, 1.0);
+      glDisable(GL_LIGHTING);
+      glutWireCube(10);
+      glEnable(GL_LIGHTING);
+
+      glTranslatef(0, 0, 5);
+      glColor3f (1.0, 1.0, 1.0);
+      glutSolidSphere(2, 50, 50);
+   glPopMatrix();
 }
 
-void reshape (int w, int h){
-   width = w;
-   height = h;
-   glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+void orthog(){
+    float w = width;
+    float h = height;
+    int ortho = 10;
+
+      // glOrtho(-orho, orho, -orho, orho, -200, 200);
+      if (width <= height)
+			glOrtho (-ortho, ortho, -ortho*h/w, ortho*h/w, -100.0, 100.0);
+		else
+			glOrtho (-ortho*w/h, ortho*w/h, -ortho, ortho, -100.0, 100.0);
+
+       glMatrixMode(GL_MODELVIEW); // Select The Modelview Matrix
+       glLoadIdentity();           // Inicializa com matriz identidade
 }
 
-void keyboard (unsigned char key, int x, int y){
+void mouse(int button, int state, int x, int y){
+    if(proj){
+       if ( button == GLUT_LEFT_BUTTON && state == GLUT_DOWN )
+       {
+          last_x = x;
+          last_y = y;
+       }
+       if (button == 3){
+            distanciaOrigem -= 1;
+       }
+       if (button == 4){
+            distanciaOrigem += 1;
+       }
+    }
+   if ( button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN )   {
+       proj= !proj;
+       if(!proj) printf("Projecao Ortogonal.\n");
+       else      printf("Projecao Perspectiva.\n");
+   }
+    glutPostRedisplay();
+}
+
+void motion(int x, int y ){
+    if(proj){
+       rotationY -= (float) (y - last_y);
+       rotationX -= (float) (x - last_x);
+       last_x = x;
+       last_y = y;
+    }
+   glutPostRedisplay();
+}
+
+void keyboard(unsigned char key, int x, int y){
    switch (key){
-       case 'a':
-            posicaoCameraX += 2;
-            break;
-       case 'd':
-            posicaoCameraX -= 2;
-            break;
-       case 'w':
-            alturaVisao += 0.02;
-            break;
-       case 's':
-            alturaVisao -= 0.02;
-            break;
-      case 27:
+      case 27 :
          exit(0);
       break;
    }
    glutPostRedisplay();
-}
-
-void mouse(int button, int state, int x, int y){
-   if ( button == GLUT_LEFT_BUTTON && state == GLUT_DOWN ){
-      last_x = x;
-      last_y = y;
-   }
-   if (button == 3){
-        posicaoCameraZ+=0.3;
-   }
-   if (button == 4){
-        posicaoCameraZ-=0.3;
-   }
-   glutPostRedisplay();
-}
-
-void motion(int x, int y){
-   last_x = x;
-   last_y = y;
 }
